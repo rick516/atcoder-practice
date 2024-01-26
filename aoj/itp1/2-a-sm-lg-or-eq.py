61229302fcc44aea4b1d2e15eb3485597fb73c5c
rick516 <k.murakami516@gmail.com>
2024-01-26 18:25:49 +0900

    add: change dir name and 2-a and b

---
+++ aoj/itp1/2-a-sm-lg-or-eq.py
@@ -0,0 +1,11 @@
+i_list = input().split(" ")
+
+a = int(i_list[0])
+b = int(i_list[1])
+
+if (a > b):
+  print("a > b")
+elif (a < b):
+  print("a < b")
+else:
+  print("a == b")
---
+++ aoj/itp1/2-b-range.py
@@ -0,0 +1,10 @@
+list = input().split(" ")
+a = int(list[0])
+b = int(list[1])
+c = int(list[2])
+
+if (a < b < c):
+  print("Yes")
+else:
+  print("No")
+
